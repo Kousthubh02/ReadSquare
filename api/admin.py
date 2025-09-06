fef03935b1674837b994cb2053a2404a3fcf16ea
kousthubh <kyadavalli04@gmail.com>
2025-09-06 12:10:51 +0530

    models created and registered

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,116 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Category , Author , Book , Customer,Cart,CartItem,Wishlist,Review 
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active', 'created_at']
+    list_filter = ['is_active','created_at']
+    search_fields=['name','description']
+    prepopulated_fields={'slug':('name',)}
+    list_editable=['is_active']
+    
+    def book_count(self,obj):
+        return obj.books.count()
+    book_count.short_description='Books'
+    
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display=['name','book_count','created_at']
+    search_fields=['name','bio']
+    prepopulated_fields={'slug':('name',)}
+    
+    def book_count(self,obj):
+        return obj.books.count()
+    book_count.short_description='Books'
+    
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display=['title','author','category','format','price','stock_quantity','is_active','is_featured']
+    list_filter=['is_active','is_featured','format','category','author','created_at']
+    search_fields=['title','isbn','description','author__name']
+    prepopulated_fields={'slug':('title',)}
+    list_editable=['price','stock_quantity','is_active','is_featured']
+    readonly_fields=['created_at','updated_at','average_rating']
+    fieldsets=(
+        ('Basic Information',{
+            'fields':('title','slug','author','category','description')
+        }),
+        ('Publication Details',{
+            'fields':('isbn''publication_date','pages','format')
+        }),  
+        ('Pricing & Inventory',{
+            'fields': ('price','original_price','stock_quantity')
+        }),
+        ('Image',{
+            'fields':('cover_image',)
+        }),
+        ('Status',{
+            'fields':('is_active','is_featured')
+        }),
+        ('Info',{
+            'fields':('average_rating','created_at','updated_at'),
+            'classes':('collapse',)
+        }),
+        )
+    
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display=['full_name','user_email','phone','city','created_at']
+    search_fields=['user_username','user_email','user_first_name','user_last_name','phone']
+    readonly_fields=['created_at']
+    
+    def user_email(self,obj):
+        return obj.user.email
+    user_email.short_description='Email'
+    
+class CartItemInLine(admin.TabularInline):
+    model=CartItem
+    extra=0
+    readonly_fields=['total_price']
+    
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display=['customer_name','total_items','total_price','created_at']
+    list_filter=['created_at']
+    search_fields=['customer__user__username','session_key']
+    inLines=[CartItemInLine]
+    readonly_fields=['total_items','total_price',]
+    
+    def customer_name(self,obj):
+        if obj.customer:
+            return obj.customer.user.username
+        return f"Anonymous ({obj.session_key})"
+    customer_name.short_description='Customer'
+    
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display=['cart_customer','book','quantity','total_price','created_at']
+    list_filter=['created_at']
+    search_fields=['cart__customer__user__username','book__title']
+    readonly_fields=['total_price']
+    
+    def cart_customer(self,obj):
+        if obj.cart.customer:
+            return obj.cart.customer.user.username
+        return f"Anonymous"
+    cart_customer.short_description='Customer'
+    
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display=['customer','book','created_at']
+    list_filter=['created_at']
+    search_fields=['customer__user__username','book__title']
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display=['book','customer','rating','is_approved','created_at']
+    list_filter=['rating','is_approved','created_at']
+    search_fields=['book__title','customer__user__username','comment']
+    list_editable=['is_approved']
+    readonly_fields=['created_at']
+    
+    
+    
+    
--- api/models.py
+++ api/models.py
@@ -1,3 +1,169 @@
+from wsgiref.validate import validator
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.urls import reverse
+from decimal import Decimal 
 
-# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100, unique=True)
+    slug=models.SlugField(max_length=100, unique=True)
+    description=models.TextField(blank=True)
+    is_active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name_plural="Categories"
+        ordering=['name']
+        
+    def __str__(self):
+        return self.name 
+    
+class Author(models.Model):
+    name=models.CharField(max_length=100)
+    slug=models.SlugField(max_length=100, unique=True)
+    bio=models.TextField(blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['name']
+    
+    def __str__(self):
+        return self.name
+    
+class Book(models.Model):
+    BOOK_FORMATS=[
+        ('HARDCOVER','Hardcover'),
+        ('PAPERBACK','Paperback'),
+        ('EBOOK','eBook'),
+    ]
+    
+    title=models.CharField(max_length=200)
+    slug=models.SlugField(max_length=200, unique=True)
+    isbn=models.CharField(max_length=13, unique=True,blank=True,null=True)
+    
+    author=models.ForeignKey(Author , on_delete=models.CASCADE, related_name='books')
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, null=True, related_name='books')
+  
+    description=models.TextField(blank=True)
+    pages=models.PositiveIntegerField(null=True,blank=True)
+    publication_date=models.DateField()
+    format=models.CharField(max_length=20, choices=BOOK_FORMATS)
+    
+    cover_image=models.ImageField(upload_to='books/covers/',null=True,blank=True)
+    
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    original_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    
+    is_active=models.BooleanField(default=True)
+    is_featured=models.BooleanField(default=False)
+    
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    
+    
+    class Meta:
+        ordering=['-created_at']
+        
+    def __str__(self):
+        return self.title
+    def get__absolute_url(self):
+        return reverse('book_detail',args=[self.slug])
+    
+    
+    @property
+    def discount_percentage(self):
+        if self.original_price and self.original_price>self.price:
+            return int(((self.original_price - self.price)/self.original_price)*100)
+        return 0
+    
+    @property
+    def is_in_stock(self):
+        return self.stock_quantity>0
+    
+    @property
+    def average_rating(self):
+        reviews=self.reviews.filter(is_approved=True)
+        if reviews.exists():
+            return round(reviews.aggregate(models.Avg('rating'))['rating__avg'],1)
+        return None
+    
+class Customer(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    phone=models.CharField(max_length=15,blank=True,null=True)
+    address=models.TextField(blank=True,null=True)
+    city=models.CharField(max_length=100,blank=True)
+    postal_code=models.CharField(max_length=20,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.get_full_name()}'s Profile"
+    
+    @property
+    def full_name(self):
+        return f"{self.user.first_name} {self.user.last_name}"
+    
+class Cart(models.Model):
+    customer=models.OneToOneField(Customer,on_delete=models.CASCADE,null=True,blank=True)
+    session_key=models.CharField(max_length=40,null=True,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        if self.customer:
+            return f"{self.customer.user.get_full_name()}'s Cart"
+        return f"Anonymous Cart {self.session_key}"
+    
+    @property
+    def total_items(self):
+        return sum(item.quantity for item in self.items.all())
+    
+    @property
+    def total_price(self):
+        return sum(item.total_price for item in self.items.all())
+
+class CartItem(models.Model):
+    cart=models.ForeignKey(Cart,on_delete=models.CASCADE, related_name='items')
+    book=models.ForeignKey(Book,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1,validators=[MinValueValidator(1)])
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        unique_together=['cart','book']
+        
+    def __str__(self):
+        return f"{self.quantity} of {self.book.title}"
+    
+    @property
+    def total_price(self):
+        return self.quantity*self.book.price
+    
+class Wishlist(models.Model):
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE,related_name='wishlists')
+    book=models.ForeignKey(Book,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+        
+    class Meta:
+        unique_together=['customer','book']
+            
+    def __str__(self):
+        return f"{self.customer.user.get_full_name()}'s Wishlist Item: {self.book.title }"
+    
+    
+class Review(models.Model):
+    book=models.ForeignKey(Book,on_delete=models.CASCADE,related_name='reviews')
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE,related_name='reviews')
+    rating=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    comment=models.TextField(blank=True)
+    is_approved=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together=['book','customer']
+        ordering=['-created_at']
+        
+    def __str__(self):
+        return f"Review of {self.book.title} by {self.customer.user.get_full_name()}"
